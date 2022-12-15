23e7c7ee6a07b16e36fcd29d3f0b029344aaeaf9
gincegeo@gmail.com <gincegeo@gmail.com>
2022-12-15 21:05:08 +0530

    banner component created

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+import "./Banner.css";
+import axios, { all } from "axios";
+import { API_KEY, baseURL, imageUrl } from "../constants/constants";
+
+const Banner = () => {
+  const [movie, setMovie] = useState();
+  useEffect(() => {
+    let randomNumer = Math.floor(Math.random() * (19 - 0) + 0);
+    axios
+      .get(`${baseURL}/trending/all/week?api_key=${API_KEY}&language=en-US`)
+      .then((responce) => {
+        setMovie(responce.data.results[randomNumer]);
+        console.log(responce.data.results[randomNumer]);
+      });
+  }, []);
+
+  return (
+    <div
+      className="banner"
+      style={{ backgroundImage: `url(${movie ? imageUrl+movie.backdrop_path : ""})` }}
+    >
+      <div className="content">
+        <h1 className="title">{movie ? movie.title : "" }</h1>
+        <div className="banner_buttons">
+          <button className="button">Play</button>
+          <button className="button">My list</button>
+        </div>
+        <h1 className="description">{movie ? movie.overview : "" }</h1>
+      </div>
+      <div className="fade_bottom"></div>
+    </div>
+  );
+};
+
+export default Banner;
---
+++ src/components/constants/constants.js
@@ -0,0 +1,3 @@
+export const API_KEY = "8d03cb5aebafd62641395ef125c70f3b";
+export const baseURL = "https://api.themoviedb.org/3";
+export const imageUrl = "https://image.tmdb.org/t/p/original";
